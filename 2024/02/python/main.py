989ec07c13cef3bf573ef20025a08ae04fde1890
James Blash <j.w.blash@gmail.com>
2024-12-02 19:37:10 -0500

    wip 2024 d2pt1 bad at recursion

--- 2024/02/python/main.py
+++ 2024/02/python/main.py
@@ -2,7 +2,7 @@
 
 import sys
 
-file_to_read = "input"
+file_to_read = "sample"
 
 def split_to_ints(levels):
     return [int(num) for num in levels]
@@ -12,7 +12,11 @@ def adj_check(x, y):
     return abs(x - y) <= 3 and abs(x - y) > 0
     
 
-def is_increasing_safely(levels):
+def problem_dampener(levels_no_problem):
+        return is_increasing_safely(levels_no_problem, dampen=False) or is_decreasing_safely(levels_no_problem, dampen=False)
+
+
+def is_increasing_safely(levels, dampen=True):
     safe = True
     idx = 0
     while idx < len(levels):
@@ -22,24 +26,32 @@ def is_increasing_safely(levels):
         except IndexError:
             break 
         if this >= following or not adj_check(this, following):
+            if (dampen):
+                arr = levels[:idx]+levels[idx+1:]
+                if problem_dampener(arr):
+                    continue
             safe = False
             break 
         idx += 1
     return safe 
 
 
-def is_decreasing_safely(levels):
+def is_decreasing_safely(levels, dampen=True):
     safe = True
     idx = 0
     while idx < len(levels):
         try:
             this = levels[idx]
             following = levels[idx+1]
-            if this <= following or not adj_check(this, following):
-                safe = False
-                break
         except IndexError:
             break 
+        if this <= following or not adj_check(this, following):
+            if (dampen):
+                arr = levels[:idx]+levels[idx+1:]
+                if problem_dampener(arr):
+                    continue
+            safe = False
+            break
         idx += 1
     return safe
 
