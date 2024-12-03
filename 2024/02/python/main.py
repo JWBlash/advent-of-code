a9443ca9f1c34d6c8141261d6140ced3e3107e51
James Blash <j.w.blash@gmail.com>
2024-12-03 14:42:08 -0500

    refactor

--- 2024/02/python/main.py
+++ 2024/02/python/main.py
@@ -12,15 +12,29 @@ def adj_check(x, y):
     return abs(x - y) <= 3 and abs(x - y) > 0
     
 
+def is_increasing(x, y):
+    return x >= y
+
+
+def is_decreasing(x, y):
+    return x <= y
+
+
 def problem_dampener_increasing(levels_no_problem):
-        return is_increasing_safely(levels_no_problem, dampen=False)
+        return is_safe(levels_no_problem, increasing=True, dampen=False)
 
 
 def problem_dampener_decreasing(levels_no_problem):
-        return is_decreasing_safely(levels_no_problem, dampen=False)
+        return is_safe(levels_no_problem, increasing=False, dampen=False)
 
 
-def is_increasing_safely(levels, dampen=True):
+def is_safe(levels, increasing=True, dampen=True):
+    if increasing:
+        safety_func = is_increasing
+        dampen_func = problem_dampener_increasing
+    else:
+        safety_func = is_decreasing 
+        dampen_func = problem_dampener_decreasing
     safe = True
     idx = 0
     while idx < len(levels):
@@ -29,36 +43,14 @@ def is_increasing_safely(levels, dampen=True):
             next = levels[idx+1]
         except IndexError:
             break 
-        if this >= next or not adj_check(this, next):
+        if safety_func(this, next) or not adj_check(this, next):
             safe = False
         if not safe and dampen:
             without_this = levels.copy()
             del without_this[idx]
             without_next = levels.copy()
             del without_next[idx+1]
-            if problem_dampener_increasing(without_this) or problem_dampener_increasing(without_next):
-                safe = True
-        idx += 1
-    return safe 
-
-
-def is_decreasing_safely(levels, dampen=True):
-    safe = True
-    idx = 0
-    while idx < len(levels):
-        try:
-            this = levels[idx]
-            next = levels[idx+1]
-        except IndexError:
-            break 
-        if this <= next or not adj_check(this, next):
-            safe = False
-        if not safe and dampen:
-            without_this = levels.copy()
-            del without_this[idx]
-            without_next = levels.copy()
-            del without_next[idx+1]
-            if problem_dampener_decreasing(without_this) or problem_dampener_decreasing(without_next):
+            if dampen_func(without_this) or dampen_func(without_next):
                 safe = True
         idx += 1
     return safe
@@ -72,7 +64,7 @@ with open(file_to_read, "r") as f:
     for report in content:
         levels = report.split(" ")
         levels = split_to_ints(levels)
-        if is_increasing_safely(levels) or is_decreasing_safely(levels):
+        if is_safe(levels) or is_safe(levels, increasing=False):
             num_safe += 1
 
 print(num_safe)
