184aab3b396d1213f804b6b3d0d5ad9a366d74bc
jwblash <j.w.blash@gmail.com>
2024-12-14 13:15:24 -0500

    wip pt2

--- 2024/03/python/main.py
+++ 2024/03/python/main.py
@@ -9,7 +9,15 @@ matches = []
 with open(file_to_read, "r") as f:
     content = f.readlines()
     for line in content:
-        matches += re.findall(r"mul\(\d+\,\d+\)", line)
+        doline = re.split(r"do\(\)", line)
+        for l in doline:
+            valid = l
+            dont = re.search(r"don't\(\)", l)
+            if dont:
+                disregard = l[dont.span()[1]:]
+                valid = l[:dont.span()[0]]
+            more = re.findall(r"mul\(\d+\,\d+\)", valid)
+            matches += more
 
 sum = 0
 
@@ -21,5 +29,3 @@ for match in matches:
     sum += int(x) * int(y)
 
 print(sum)
-    
-
